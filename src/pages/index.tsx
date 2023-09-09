917e6a0e1402bcc4bda19a6d1fff9cd282e4c06f
Carlos && Ponce <srtager555@gmail.com>
2023-09-09 15:25:05 -0600

    logic to print the featured products and searched products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,26 @@ import { Container, ProductContainer } from "@/styles/index.styles";
 import { Nav } from "@/components/Nav";
 import { Firestore } from "@/tools/firestore";
 import { ProductCard } from "@/components/product.card";
+import { searchProduct } from "@/tools/searchProduct";
 
 export default function Home() {
 	const [products, setProducts] = useState<product[]>([]);
+	const [featured, setFeatured] = useState<product[]>([]);
+	const [entryResult, setEntryResult] = useState<product[]>([]);
+	const [entry, setEntry] = useState<string>("");
 	const db = Firestore();
+
+	useEffect(() => {
+		const result = searchProduct(entry, products);
+
+		setEntryResult(result);
+	}, [entry, products]);
+
+	useEffect(() => {
+		if (products.length > 0) {
+			setFeatured(products.filter((el) => el.featured));
+		}
+	}, [products]);
 
 	useEffect(() => {
 		const collectionProducts = collection(db, "/products/") as CollectionReference<product>;
@@ -27,18 +43,28 @@ export default function Home() {
 
 	return (
 		<>
-			<Nav />
+			<Nav setEntry={setEntry} />
 			<Container>
 				<ProductContainer>
-					{products.map((el, index) => (
-						<ProductCard
-							key={index}
-							name={el.name}
-							weight={el.weight}
-							price={el.price}
-							imagePath={el.imagePath}
-						/>
-					))}
+					{entryResult.length > 0
+						? entryResult.map((el, index) => (
+								<ProductCard
+									key={index}
+									name={el.name}
+									weight={el.weight}
+									price={el.price}
+									imagePath={el.imagePath}
+								/>
+						  ))
+						: featured.map((el, index) => (
+								<ProductCard
+									key={index}
+									name={el.name}
+									weight={el.weight}
+									price={el.price}
+									imagePath={el.imagePath}
+								/>
+						  ))}
 				</ProductContainer>
 			</Container>
 		</>
