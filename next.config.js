2b0155bb8d17fad619d9646810a6600020e1b212
Carlos && Ponce <srtager555@gmail.com>
2023-09-08 21:14:26 -0600

    config updated to use styled component and images from the storage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "9199", // fb storage btw
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
