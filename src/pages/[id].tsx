5dbf8c0025f8e0b3c2f6e94bb0460a1d4b661bf9
Carlos && Ponce <srtager555@gmail.com>
2023-09-08 19:44:44 -0600

    fancy redirect

---
+++ src/pages/[id].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Redirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, [router]);
+
+  return <>redireccionando...</>;
+}
