6d0b045097eaa7174183d44368508d7632b55621
Carlos && Ponce <srtager555@gmail.com>
2023-09-08 23:19:05 -0600

    First Product Card Version created

---
+++ src/components/product.card.tsx
@@ -0,0 +1,69 @@
+import { Bebas_Neue, Roboto } from "next/font/google";
+import Image from "next/image";
+import styled from "styled-components";
+
+const BebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"] });
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+const Card = styled.div`
+	width: 300px;
+`;
+
+const TitleBox = styled.div`
+	/* width: 300px; */
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+`;
+
+const Title = styled.h3`
+	display: inline;
+	font-size: 2rem;
+	margin: 0;
+`;
+
+const Weight = styled.span`
+	font-size: 1.5rem;
+`;
+
+const Price = styled.span`
+	display: block;
+	text-align: end;
+`;
+
+const ImageBox = styled.div`
+	position: relative;
+	width: 300px;
+	height: 300px;
+`;
+
+interface props {
+	name: string;
+	price: number;
+	weight: number;
+	imagePath: string;
+}
+
+export function ProductCard({ name, price, weight, imagePath }: props) {
+	return (
+		<Card>
+			<Price className={roboto.className}>LPS {price}</Price>
+			<TitleBox>
+				<Title className={BebasNeue.className}>{name}</Title>
+				<Weight className={BebasNeue.className}>
+					{weight} {weight > 1 ? "LBS" : "LB"}
+				</Weight>
+			</TitleBox>
+			<ImageBox>
+				<Image
+					src={
+						imagePath ??
+						"https://i.ibb.co/VN0q6r2/9-C4-FB247-B3067-AE137-DACCFFBF98-E5-C2-DF85505-D.jpg"
+					}
+					fill
+					alt={name}
+				/>
+			</ImageBox>
+		</Card>
+	);
+}
