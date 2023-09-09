9f421d6fe9705db305592186045d0e8f70975c5d
Carlos && Ponce <srtager555@gmail.com>
2023-09-08 21:15:13 -0600

    [prototype] first home page syntax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react";
+import {
+	getFirestore,
+	collection,
+	onSnapshot,
+	DocumentData,
+	CollectionReference,
+} from "firebase/firestore";
+import { Container } from "@/styles/index.styles";
+
 export default function Home() {
-  return <>empty</>;
+	const [products, setProducts] = useState<product[]>([]);
+	const db = getFirestore();
+
+	useEffect(() => {
+		const collectionProducts = collection(db, "/products/") as CollectionReference<product>;
+
+		const unsubcribe = onSnapshot(collectionProducts, (snap) => {
+			snap.forEach((doc) => {
+				setProducts((prev) => [...prev, doc.data()]);
+			});
+		});
+
+		return () => {
+			unsubcribe();
+		};
+	}, [db]);
+
+	return (
+		<Container>
+			{products.map((el) => (
+				<>el.name</>
+			))}
+		</Container>
+	);
 }
