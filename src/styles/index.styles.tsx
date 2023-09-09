0e0cdd4788ab1a4f56e26ee8e2dcd9054d02732b
Carlos && Ponce <srtager555@gmail.com>
2023-09-08 23:19:57 -0600

    ProfuctCard Container

--- src/styles/index.styles.tsx
+++ src/styles/index.styles.tsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 20px;
-  width: 100%;
+	padding: 20px;
+	width: 100%;
+`;
+
+export const ProductContainer = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(5, 300px);
+	gap: 50px;
 `;
