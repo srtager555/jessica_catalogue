ce3d3138d5a1b813f51c8f443102c5d5f2977cfb
Carlos && Ponce <srtager555@gmail.com>
2023-09-10 00:16:30 -0600

    prototype o f the form added

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,18 +1,11 @@
-import { LogIn } from "@/components/Login";
-import { Nav } from "@/components/admin/Nav";
-import { useAuthUserListener } from "@/hooks/useAuth";
-import { Container } from "@/styles/index.styles";
-import { getAuth } from "firebase/auth";
-import { useEffect } from "react";
-
-export default function Admin() {
-	const auth = useAuthUserListener();
-
-	if (!auth) return <LogIn />;
-
+export default function Add() {
 	return (
-		<Container>
-			<Nav />
-		</Container>
+		<div>
+			<form>
+				<input placeholder="nombre" />
+				<input placeholder="precio" />
+				<input placeholder="peso" />
+			</form>
+		</div>
 	);
 }
