3d621ecc6ab61a9e483c9b5516a4dba20e5e15ab
Carlos && Ponce <srtager555@gmail.com>
2023-09-09 20:43:40 -0600

    context created

--- src/layout/admin.tsx
+++ src/layout/admin.tsx
@@ -1,15 +1,22 @@
 import { LogIn } from "@/components/Login";
 import { Nav } from "@/components/admin/Nav";
+import { ProductList } from "@/components/productList";
 import { useAuthUserListener } from "@/hooks/useAuth";
 import { Container } from "@/styles/index.styles";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, createContext, useEffect, useState } from "react";
 
 interface props {
 	children: children;
 }
 
+export const AdminContext = createContext<{
+	productSelector: string | undefined;
+	setProductSelector: Dispatch<SetStateAction<string | undefined>>;
+} | null>(null);
+
 export function AdminLayout({ children }: props) {
+	const [productSelector, setProductSelector] = useState<string>();
 	const [show, setShow] = useState(false);
 	const router = useRouter();
 	const auth = useAuthUserListener();
@@ -35,9 +42,12 @@ export function AdminLayout({ children }: props) {
 	if (show && !auth) return <LogIn />;
 
 	return (
-		<Container>
-			<Nav />
-			{children}
-		</Container>
+		<AdminContext.Provider value={{ productSelector, setProductSelector }}>
+			<Container>
+				<Nav />
+				<ProductList setProductSelector={setProductSelector} />
+				{children}
+			</Container>
+		</AdminContext.Provider>
 	);
 }
