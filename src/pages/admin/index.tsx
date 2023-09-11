71943e1d0f968ea6947a923f681cef679077afd5
Carlos && Ponce <srtager555@gmail.com>
2023-09-10 22:01:36 -0600

    I starting to add styles

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,11 +1,64 @@
+import { Title } from "@/styles/index.styles";
+import styled from "styled-components";
+import { Roboto } from "next/font/google";
+import { InputImage } from "@/components/InputImage";
+import { useState } from "react";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+const Box = styled.div`
+	width: 60%;
+	font-family: ${roboto.style.fontFamily};
+	font-style: ${roboto.style.fontStyle};
+	font-weight: ${roboto.style.fontWeight};
+`;
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Names = styled.p`
+	text-align: end;
+	margin-bottom: 20px;
+	margin-right: 20px;
+`;
+
+const Input = styled.input`
+	display: block;
+	margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+	width: 100%;
+	padding: 30px;
+	background-color: #fff;
+	border-radius: 20px;
+`;
+
 export default function Add() {
+	const [imageURl, setImageUrl] = useState<any>();
+
+	console.log(imageURl);
+
 	return (
-		<div>
-			<form>
-				<input placeholder="nombre" />
-				<input placeholder="precio" />
-				<input placeholder="peso" />
-			</form>
-		</div>
+		<Box>
+			<Title>Añadir producto</Title>
+			<Form>
+				<Container>
+					<div>
+						<Names>Nombre</Names>
+						<Names>Precio</Names>
+						<Names>Peso</Names>
+					</div>
+					<div>
+						<Input />
+						<Input />
+						<Input />
+					</div>
+					<InputImage setImageUrl={setImageUrl} />
+				</Container>
+				<div></div>
+			</Form>
+		</Box>
 	);
 }
