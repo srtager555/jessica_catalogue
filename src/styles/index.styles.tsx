9b2371024b844de990809bf7f22fba08aad1974d
Carlos && Ponce <srtager555@gmail.com>
2023-09-10 22:01:58 -0600

    recycled to the InputImage

--- src/styles/index.styles.tsx
+++ src/styles/index.styles.tsx
@@ -1,7 +1,20 @@
+/* eslint-disable react/display-name */
 import { Bebas_Neue } from "next/font/google";
-import styled from "styled-components";
+import Image, { ImageProps } from "next/image";
+import { MutableRefObject, forwardRef, LegacyRef } from "react";
+import styled, {
+	CSSObject,
+	CSSProperties,
+	DefaultTheme,
+	ThemedStyledProps,
+	css,
+} from "styled-components";
+import { Roboto } from "next/font/google";
+import { color } from "@/common/colors";
 
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 const BebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"] });
+const BTNDefaultPadding = { padding: "9px 12px" };
 
 export const Container = styled.div`
 	padding: 20px;
@@ -46,3 +59,162 @@ export const Title = styled.h2`
 	font-weight: ${BebasNeue.style.fontWeight};
 	font-style: ${BebasNeue.style.fontStyle};
 `;
+
+// Recycled
+
+// Function to process props and convert them into valid css styles
+const processStyles = (
+	props: ThemedStyledProps<{ styles?: CSSProperties; children?: children }, DefaultTheme>
+): CSSObject => {
+	const styleProps: CSSObject = {};
+
+	if (!props.styles) return styleProps;
+
+	// checking if the key is valid
+	Object.keys(props.styles).forEach((key) => {
+		// the camel case will process to the correct syntax, for example: maxWidth => max-width
+		const cssKey = key.replace(/([A-Z])/g, "-$1").toLowerCase();
+		styleProps[cssKey as string] =
+			props.styles && props.styles[key as keyof typeof props.styles];
+	});
+
+	return styleProps;
+};
+
+/* this function will delete values that I don't want to overwrite when
+ I use the destructuring */
+function deleteRepeatedValues(props: object) {
+	const attributesToDelete = ["children", "font", "className", "theme", "styles"];
+	const clone: { [key: string]: unknown } = { ...props };
+
+	for (const attribute in clone) {
+		// eslint-disable-next-line no-prototype-builtins
+		if (clone.hasOwnProperty(attribute) && attributesToDelete.includes(attribute)) {
+			delete clone[attribute];
+		}
+	}
+
+	return clone;
+}
+
+interface DivType extends React.HTMLAttributes<HTMLDivElement> {
+	font?: string;
+	ref?: MutableRefObject<HTMLDivElement>;
+}
+
+// eslint-disable-next-line react/display-name
+const Div = forwardRef((props: DivType, ref: LegacyRef<HTMLDivElement> | undefined) => {
+	const { children, font, className } = props;
+	const clone = deleteRepeatedValues(props);
+
+	return (
+		<div ref={ref} className={`${className} ${font ?? roboto.className}`} {...clone}>
+			{children}
+		</div>
+	);
+});
+
+export const StyledDiv = styled(Div)<{ styles?: CSSProperties }>((props) => ({
+	position: props.styles?.position || "relative",
+	...processStyles(props),
+}));
+
+interface ButtonType extends React.HTMLAttributes<HTMLButtonElement> {
+	font?: string;
+}
+
+// main button
+
+const ButtonXDXD = forwardRef(
+	(props: ButtonType, ref: LegacyRef<HTMLButtonElement> | undefined) => {
+		const { children, font, className } = props;
+		const clone = deleteRepeatedValues(props);
+
+		return (
+			<button ref={ref} className={`${className} ${font ?? roboto.className}`} {...clone}>
+				{children}
+			</button>
+		);
+	}
+);
+
+export const StyledButton = styled(ButtonXDXD)<{ styles?: CSSProperties; children?: children }>`
+	${(props) => ({
+		position: props.styles?.position || "relative",
+		display: "inline-block",
+		color: "inherit",
+		backgroundColor: "transparent",
+		borderRadius: "10px",
+		border: "none",
+		transition: "200ms",
+		cursor: "pointer",
+		...processStyles(props),
+	})}
+
+	&:active {
+		background-color: ${color["active-btn"]};
+		transform: scale(0.95);
+	}
+
+	&:disabled {
+		opacity: 0.2;
+		cursor: not-allowed;
+	}
+`;
+
+interface props extends React.HTMLAttributes<HTMLButtonElement> {
+	styles?: CSSProperties;
+	disabled?: boolean;
+}
+export const ButtonNormal = styled(({ children, className, styles, ...props }: props) => (
+	<StyledButton className={className} styles={{ ...styles, ...BTNDefaultPadding }} {...props}>
+		{children}
+	</StyledButton>
+))`
+	${(props: { styles?: CSSProperties; $primary?: boolean; $warn?: boolean }) => {
+		if (props.$primary)
+			return css`
+				color: ${props.styles?.color ?? "#fff"};
+				background-color: ${props.styles?.backgroundColor ?? color.primary};
+			`;
+		else if (props.$warn)
+			return css`
+				color: ${props.styles?.color ?? "#fff"};
+				background-color: ${props.styles?.backgroundColor ?? color.main};
+			`;
+		else
+			return css`
+				color: ${props.styles?.color ?? "#000"};
+				background-color: {props.styles?.backgroundColor ?? "transparent"};
+				border: ${props.styles?.border ?? `1px solid ${color.main}`};
+			`;
+	}}
+`;
+
+export const ContainerGOD = styled(StyledDiv)``;
+
+export const SpaceBeetwenContainer = styled(StyledDiv)`
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+`;
+
+export const FlexContainer = styled(StyledDiv)`
+	display: flex;
+`;
+
+const Square = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+`;
+
+export const SquareImage = (props: ImageProps) => (
+	<Square>
+		<Image {...props} fill style={{ objectFit: "cover" }} alt={props.alt} />
+	</Square>
+);
