2e747f3f768d5f12b19e322fd0f2115bc0d0b70d
Carlos && Ponce <srtager555@gmail.com>
2023-09-13 21:42:26 -0600

    form to add a new category moved to a new file

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,11 @@
-import { FlexContainer, Title } from "@/styles/index.styles";
+import { FlexContainer, Form, Title } from "@/styles/index.styles";
 import styled from "styled-components";
 import { Roboto } from "next/font/google";
 import { InputImage } from "@/components/InputImage";
 import { FormEvent, HTMLAttributes, useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs } from "firebase/firestore";
 import { Firestore } from "@/tools/firestore";
+import { CreateCategory } from "@/components/admin/createCat.form";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
@@ -38,35 +39,11 @@ const Input = styled.input<{ m?: boolean }>`
 	margin-bottom: ${({ m }) => (m ? "0" : "20px")};
 `;
 
-const Form = styled.form`
-	width: 100%;
-	padding: 30px;
-	margin-bottom: 20px;
-	background-color: #fff;
-	border-radius: 20px;
-`;
-
 export default function Add() {
 	const [imageURl, setImageUrl] = useState<any>();
 	const db = Firestore();
 
 	console.log(imageURl);
-
-	async function addCategory(e: FormEvent) {
-		e.preventDefault();
-
-		const target = e.currentTarget as typeof e.currentTarget & {
-			cate: HTMLInputElement;
-		};
-
-		if (target.cate.value === "" || target.cate.value === undefined) return;
-
-		const coll = collection(db, "/categories");
-
-		await addDoc(coll, {
-			name: target.cate.value,
-		});
-	}
 
 	return (
 		<Box>
@@ -92,13 +69,7 @@ export default function Add() {
 					<option>Categoria</option>
 				</select>
 			</Form>
-			<Title>Crear una nueva Categoria</Title>
-			<Form onSubmit={addCategory}>
-				<FlexContainer {...flexProps}>
-					<Input m name="cate" />
-					<button>Crear</button>
-				</FlexContainer>
-			</Form>
+			<CreateCategory />
 			<Title>Eliminar una Categoria</Title>
 			<Form>
 				<FlexContainer {...flexProps}>
