7a4f0fbfb2fc76e9241a9caa0481ffec7537a2f6
Carlos && Ponce <srtager555@gmail.com>
2023-09-16 21:01:31 -0600

    logic to show the product image ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, CollectionReference } from "firebase/firestore";
+import {
+	collection,
+	onSnapshot,
+	CollectionReference,
+	QueryDocumentSnapshot,
+	DocumentData,
+} from "firebase/firestore";
 import { Container, ProductContainer } from "@/styles/index.styles";
 import { Nav } from "@/components/Nav";
 import { Firestore } from "@/tools/firestore";
 import { ProductCard } from "@/components/product.card";
 import { searchProduct } from "@/tools/searchProduct";
 import { useGetProducts } from "@/hooks/useGetProducts";
+import { getImage } from "@/tools/storage/getImage";
+
+type p = QueryDocumentSnapshot<product, DocumentData>[];
 
 export default function Home() {
-	const [products, setProducts] = useState<product[]>([]);
-	const [featured, setFeatured] = useState<product[]>([]);
-	const [entryResult, setEntryResult] = useState<product[]>([]);
+	const [products, setProducts] = useState<p>([]);
+	const [featured, setFeatured] = useState<p>([]);
+	const [entryResult, setEntryResult] = useState<p>([]);
 	const [entry, setEntry] = useState<string>("");
 	const productsListener = useGetProducts();
 	const db = Firestore();
@@ -23,14 +32,14 @@ export default function Home() {
 
 	useEffect(() => {
 		if (products.length > 0) {
-			const featured = products.filter((el) => el.featured);
+			const featured = products.filter((el) => el.data().featured);
 
 			if (featured.length < 10) {
 				const sortedProducts = products.sort((a, b) => {
-					if (a.name > b.name) {
+					if (a.data().name > b.data().name) {
 						return 1;
 					}
-					if (a.name < b.name) {
+					if (a.data().name < b.data().name) {
 						return -1;
 					}
 					// a must be equal to b
@@ -45,8 +54,8 @@ export default function Home() {
 	}, [products]);
 
 	useEffect(() => {
-		setProducts(productsListener.data);
-	}, [productsListener.data]);
+		setProducts(productsListener.snap);
+	}, [productsListener.snap]);
 
 	return (
 		<>
@@ -55,27 +64,43 @@ export default function Home() {
 				<ProductContainer>
 					{entryResult.length > 0
 						? entryResult.map((el, index) => (
-								<ProductCard
-									key={index}
-									name={el.name}
-									weight={el.weight}
-									price={el.price}
-									brand={el.brand}
-									imagePath={el.imagePath}
-								/>
+								<CardRender key={index} el={el} index={index} />
 						  ))
 						: featured.map((el, index) => (
-								<ProductCard
-									key={index}
-									name={el.name}
-									weight={el.weight}
-									brand={el.brand}
-									price={el.price}
-									imagePath={el.imagePath}
-								/>
+								<CardRender key={index} el={el} index={index} />
 						  ))}
 				</ProductContainer>
 			</Container>
 		</>
+	);
+}
+
+function CardRender({
+	el,
+	index,
+}: {
+	el: QueryDocumentSnapshot<product, DocumentData>;
+	index: number;
+}) {
+	const [image, setImage] = useState<string>();
+	const data = el.data();
+
+	useEffect(() => {
+		async function gImage() {
+			setImage(await getImage(`products/${el.id}/product`));
+		}
+
+		gImage();
+	}, [el.id]);
+
+	return (
+		<ProductCard
+			key={index}
+			name={data.name}
+			weight={data.weight}
+			price={data.price}
+			brand={data.brand}
+			imagePath={image}
+		/>
 	);
 }
