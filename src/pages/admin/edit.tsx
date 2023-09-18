3fe4293aaa4391011f383409cb044b41c5415a9f
Carlos && Ponce <srtager555@gmail.com>
2023-09-17 20:22:00 -0600

    styles added and edit state added in the form

--- src/pages/admin/edit.tsx
+++ src/pages/admin/edit.tsx
@@ -1,13 +1,24 @@
 import { FormProduct } from "@/components/admin/form.product";
 import { Title } from "@/styles/index.styles";
+import { Roboto } from "next/font/google";
+import styled from "styled-components";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+const Box = styled.div`
+	width: 60%;
+	font-family: ${roboto.style.fontFamily};
+	font-style: ${roboto.style.fontStyle};
+	font-weight: ${roboto.style.fontWeight};
+`;
 
 export default function Edit() {
 	function edit() {}
 
 	return (
-		<div>
+		<Box>
 			<Title>Editar un productor</Title>
-			<FormProduct callback={edit} />
-		</div>
+			<FormProduct callback={edit} edit />
+		</Box>
 	);
 }
