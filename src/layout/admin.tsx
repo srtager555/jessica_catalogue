fbee9ec06e9407a3fde11767bcbf77d0edd7a2da
Carlos && Ponce <srtager555@gmail.com>
2023-09-17 20:21:44 -0600

    typing change to improve the code

--- src/layout/admin.tsx
+++ src/layout/admin.tsx
@@ -3,6 +3,7 @@ import { Nav } from "@/components/admin/Nav";
 import { ProductList } from "@/components/productList";
 import { useAuthUserListener } from "@/hooks/useAuth";
 import { Container } from "@/styles/index.styles";
+import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { Dispatch, SetStateAction, createContext, useEffect, useState } from "react";
 import styled from "styled-components";
@@ -18,12 +19,15 @@ interface props {
 }
 
 export const AdminContext = createContext<{
-	productSelector: string | undefined;
-	setProductSelector: Dispatch<SetStateAction<string | undefined>>;
+	productSelector: QueryDocumentSnapshot<product, DocumentData> | undefined;
+	setProductSelector: Dispatch<
+		SetStateAction<QueryDocumentSnapshot<product, DocumentData> | undefined>
+	>;
 } | null>(null);
 
 export function AdminLayout({ children }: props) {
-	const [productSelector, setProductSelector] = useState<string>();
+	const [productSelector, setProductSelector] =
+		useState<QueryDocumentSnapshot<product, DocumentData>>();
 	const [show, setShow] = useState(false);
 	const router = useRouter();
 	const auth = useAuthUserListener();
