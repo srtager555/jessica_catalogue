71decebe32976057cc30f4e77149c5e8f9431ebe
Carlos && Ponce <srtager555@gmail.com>
2023-09-24 23:39:35 -0600

    page added

---
+++ src/pages/xd.tsx
@@ -0,0 +1,10 @@
+import { lol } from "@/tools/product.create";
+
+export default function Xd() {
+	function perrolol() {
+		console.log("perro lol");
+		lol();
+	}
+
+	return <button onClick={perrolol}>?</button>;
+}
